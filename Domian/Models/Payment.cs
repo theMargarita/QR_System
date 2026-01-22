cefcef20189eb16427008e9bfc996192f01e5f3f
theMargarita <margotvoistinova@hotmail.se>
2026-01-22 10:56:13 +0100

    added models

---
+++ Domian/Models/Payment.cs
@@ -0,0 +1,25 @@
+﻿namespace Domain.Models
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+        public PaymentMethod Method { get; set; } //som kvitto?
+        public string? Note { get; set; } //som kvitto?
+
+        //navigation property
+        public UserTab? Tab { get; set; }
+        public int? TabId { get; set; }
+
+        public User? User { get; set; }
+        public int? UserId { get; set; }
+    }
+
+
+    public enum PaymentMethod
+    {
+        Cash = 1,
+        Swish = 2,
+    }
+}
