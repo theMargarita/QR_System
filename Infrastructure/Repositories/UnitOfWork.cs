ea8b5a1c4fd6148ff3544783064451730e82fc66
theMargarita <margotvoistinova@hotmail.se>
2026-01-27 15:41:29 +0100

    Refactor repositories: add UnitOfWork, remove ITabRepository

---
+++ Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,49 @@
+﻿using Domain.IRepositories;
+using Domian.IRepoistories;
+using Infrastructure.Data;
+
+namespace Infrastructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly QrDbContext _context;
+        private readonly IUserRepository _userRepository;
+        private readonly IContextRepository _contextRepository;
+        private readonly IProductRepository _productRepository;
+        private readonly IUserTabRepository _userTabRepository;
+        private readonly ITransactionRepository _transactionRepository;
+        private readonly IPaymentRepository _paymentRepository;
+        public UnitOfWork(QrDbContext context)
+        {
+            _context = context;
+        }
+
+        public IUserRepository Users => throw new NotImplementedException();
+
+        public IContextRepository Contexts => throw new NotImplementedException();
+
+        public IProductRepository Products => throw new NotImplementedException();
+
+        public IUserTabRepository UserTabs => throw new NotImplementedException();
+
+        public ITransactionRepository Transactions => throw new NotImplementedException();
+
+        public IPaymentRepository Payments => throw new NotImplementedException();
+
+        public Task<int> CompleteAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        //disposable pattern - dispose context - cleanup
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
