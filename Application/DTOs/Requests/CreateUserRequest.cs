80e00602cfee8478e653effd704286048a5747e4
theMargarita <margotvoistinova@hotmail.se>
2026-01-28 10:25:07 +0100

    added request for creating user

---
+++ Application/DTOs/Requests/CreateUserRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace Application.DTOs.Requests
+{
+    public record CreateUserRequest //DTO for creating or updating a User
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string FullName => $"{FirstName} {LastName}";
+        public string? PhoneNumber { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public IReadOnlyList<UserTabResponse>? Tabs { get; set; }
+        public IReadOnlyList<TransactionReponse>? Transactions { get; set; }
+        public IReadOnlyList<PaymentResonse>? Payments { get; set; }
+
+    }
+}
