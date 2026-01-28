3972b0181079d459ebf620416adf1c3b77dc814f
theMargarita <margotvoistinova@hotmail.se>
2026-01-28 10:25:22 +0100

    response for user

---
+++ Application/DTOs/Response/UserResponse/UserResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.DTOs.Response.UserResponse
+{
+    public record UserResponse
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string FullName => $"{FirstName} {LastName}";
+        public string? PhoneNumber { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
