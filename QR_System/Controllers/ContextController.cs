87f229becc1f451385176f439293e7bbdceb27d6
theMargarita <margotvoistinova@hotmail.se>
2026-02-04 08:15:26 +0100

    start controllers plus adding a endpoint in usercontroller

---
+++ QR_System/Controllers/ContextController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QR_System.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContextController : ControllerBase
+    {
+        //private IContextService _contextService;
+        //public ContextController(IContextService contextService)
+        //{
+        //    _contextService = contextService;
+        //}
+
+        //contex har med att göra med qr koder och platser
+
+        public Task<ActionResult> ScanQR(string qrToken)
+        {
+            //var contextPart = await _contextService.GetContextPartByQrTokenAsync(qrToken);
+            //if (contextPart == null)
+            //{
+            //    return NotFound();
+            //}
+            //return Ok(contextPart);
+            return Task.FromResult<ActionResult>(Ok());
+        }
+    }
+}
+
+//for future inspiration
+
+//Task<ActionResult<ScanResponse>> ScanQR(string qrToken)
+//Task<ActionResult<ContextResponse>> GetContextDetails(int contextId)
+//Task<ActionResult<JoinResponse>> JoinContext(int contextId, JoinRequest request)
+//Task<ActionResult> LeaveContext(int contextId, int userId)
---
+++ QR_System/Controllers/MenuController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QR_System.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuController : ControllerBase //maybe sould be call product controller but this is almost more likely a menu controller
+    {
+        //private readonly IProductService _productService;
+
+    }
+}
