500a995e416788f20794c7f670c9266fc8905a23
theMargarita <margotvoistinova@hotmail.se>
2026-02-04 15:20:21 +0100

    new migrations

--- Infrastructure/Migrations/20260204072022_3.0.Designer.cs
+++ Infrastructure/Migrations/20260204072022_3.0.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(QrDbContext))]
-    partial class QrDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260204072022_3.0")]
+    partial class _30
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -306,6 +309,9 @@ namespace Infrastructure.Migrations
 
                     b.Property<string>("PhoneNumber")
                         .HasColumnType("text");
+
+                    b.Property<int>("TaleId")
+                        .HasColumnType("integer");
 
                     b.HasKey("Id");
 
