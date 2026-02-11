7bffd6c3954f7457de29cc217d85ed29e030f874
theMargarita <margotvoistinova@hotmail.se>
2026-02-11 14:56:41 +0100

    Refactor and expand service interfaces for tabs and parts

---
+++ Application/IServices/ITransactionService.cs
@@ -0,0 +1,12 @@
+﻿using Application.DTOs.TransactionFolder;
+
+namespace Application.IServices
+{
+    public interface ITransactionService
+    {
+        Task<TransactionResponse> CreateTransactionAsync(TransactionRequest request);
+        Task<bool>Closetransaction(int transactionId);
+        Task<List<TransactionResponse>> GetTransactionsByUserTabIdAsync(int userTabId);
+        Task<List<TransactionResponse>> GetTransactionsByContextPartIdAsync(Guid contextPartId);
+    }
+}
