71f91182e0224fdf3206e452792cb63724ddd2f7
theMargarita <margotvoistinova@hotmail.se>
2026-02-13 12:04:14 +0100

    added logic for owner

---
+++ Application/IServices/IOwnerService.cs
@@ -0,0 +1,13 @@
+﻿using Application.DTOs.OwnerFolder;
+using Domain.Models;
+
+namespace QR_System.Controllers
+{
+    public interface IOwnerService
+    {
+        Task<OwnerResponse> CreateOwnerAsync(string ownerName);
+        Task<bool> RemoveOwnerAsync(Guid ownerId);
+        Task<OwnerResponse?> GetOwnerByIdAsync(Guid ownerId);
+        Task<IEnumerable<OwnerResponse>> GetAllOwnersAsync();
+    }
+}
