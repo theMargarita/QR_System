283c3f057ab90e7efccea87a482f876fc7129870
theMargarita <margotvoistinova@hotmail.se>
2026-02-16 15:54:20 +0100

    Refactor UserTab querying to use extension methods

--- Infrastructure/Extensions/UserTabExtension.cs
+++ Infrastructure/Extensions/UserTabExtension.cs
@@ -1,120 +1,102 @@
-﻿//using Domain.IRepositories;
-//using Domain.Models;
-//using Infrastructure.Data;
-//using Microsoft.EntityFrameworkCore;
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
 
-//namespace Infrastructure.Repositories
-//{
-//    public class UserTabRepository : Repository<UserTab>, IUserTabRepository
-//    {
-//        public UserTabRepository(QrDbContext context) : base(context)
-//        {
-//        }
+namespace Infrastructure.Extensions
+{
+    public static class UserTabExtensions
+    {
+        public static IQueryable<UserTab> GetAllUserTabs(this IQueryable<UserTab> query, Guid userId)
+        {
+            return query
+                .Where(ut => ut.UserId == userId)
+                .OrderByDescending(ut => ut.CreatedAt);
+        }
+        public static IQueryable<UserTab> GetAllUserTabsWithDetails(this IQueryable<UserTab> query, Guid userId)
+        {
+            return query
+                .Include(ut => ut.ContextPart)
+                    .ThenInclude(cp => cp.Context)
+                .Include(ut => ut.Transactions)
+                .Include(ut => ut.Payments)
+                .Where(ut => ut.UserId == userId)
+                .OrderByDescending(ut => ut.CreatedAt);
+        }
 
-//        //active tabs
-//        public async Task<IEnumerable<UserTab>> GetActiveAsync()
-//        {
-//            return await _dbSet
-//                .Where(ut => ut.Status == TabStatus.Open)
-//                .Include(u => u.ContextPart)
-//                .ToListAsync();
-//        }
+        public static IQueryable<UserTab> GetAllOpenUserTabs(this IQueryable<UserTab> query, Guid userId)
+        {
+            return query
+                .Include(ut => ut.ContextPart)
+                .Where(ut => ut.UserId == userId
+                          && ut.Status == TabStatus.Open)
+                .OrderByDescending(ut => ut.CreatedAt);
+        }
 
-//        public async Task<UserTab?> GetActiveByUserIdAsync(int userId)
-//        {
-//            return await _dbSet.Where(ut => ut.UserId == userId && ut.Status == TabStatus.Open).FirstOrDefaultAsync();
-//        }
+        public static IQueryable<UserTab> GetAllPaidUserTabs(this IQueryable<UserTab> query, Guid userId)
+        {
+            return query
+                .Include(ut => ut.ContextPart)
+                .Where(ut => ut.UserId == userId
+                          && ut.Status == TabStatus.Paid)
+                .OrderByDescending(ut => ut.CreatedAt);
+        }
 
-//        public async Task<UserTab?> GetActiveTabsByContextIdAsync(int contextId)
-//        {
-//            return await _dbSet.Where(ut => ut.ContextId == contextId && ut.Status == TabStatus.Open).FirstOrDefaultAsync();
-//        }
+        public static IQueryable<UserTab> GetAllClosedUserTabs(this IQueryable<UserTab> query, Guid userId)
+        {
+            return query
+                .Include(ut => ut.ContextPart)
+                .Where(ut => ut.UserId == userId
+                          && ut.Status == TabStatus.Closed)
+                .OrderByDescending(ut => ut.ClosedAt);
+        }
 
-//        //context tabs
+        public static IQueryable<UserTab> GetUserTabsToday(this IQueryable<UserTab> query, Guid userId)
+        {
+            var today = DateTimeOffset.UtcNow.Date;
+            var tomorrow = today.AddDays(1);
 
-//        public async Task<List<UserTab>> GetAllByContextIdAsync(int contextId)
-//        {
-//            return await _dbSet.Where(ut => ut.ContextId == contextId).ToListAsync();
-//        }
+            return query
+                .Include(ut => ut.ContextPart)
+                .Where(ut => ut.UserId == userId
+                          && ut.CreatedAt >= today
+                          && ut.CreatedAt < tomorrow)
+                .OrderByDescending(ut => ut.CreatedAt);
+        }
 
-//        public async Task<List<UserTab>> GetByContextIdAsync(int contextId)
-//        {
-//            return await _dbSet.Where(ut => ut.ContextId == contextId).ToListAsync();
-//        }
+        public static IQueryable<UserTab> ClosedTabs(this IQueryable<UserTab> query)
+        {
+            return query.Where(ut => ut.Status == TabStatus.Closed);
+        }
 
-//        public async Task<List<UserTab>> GetByUserIdAsync(int userId)
-//        {
-//            //var user =  await _dbSet.Include(u => u.User).Select(u => u.User).
-//            //    FirstOrDefaultAsync(u => u.Id == userId);
-//            //return user;
+        public static IQueryable<UserTab> ActiveTabForUser(this IQueryable<UserTab> query, Guid cpId, Guid userId)
+        {
+            return query
+                .Where(ut => ut.ContextPartId == cpId
+                          && ut.UserId == userId
+                          && ut.Status == TabStatus.Open);
+        }
 
-//            throw new NotImplementedException();
-//        }
+        public static IQueryable<UserTab> ActiveTabsAtTable(this IQueryable<UserTab> query, Guid contextPartId)
+        {
+            return query
+                .Include(ut => ut.User)
+                .Where(ut => ut.ContextPartId == contextPartId
+                          && ut.Status == TabStatus.Open);
+        }
 
-//        public async Task<IEnumerable<UserTab>> GetClosedAsync()
-//        {
-//            return await _dbSet.Where(ut => ut.Status == TabStatus.Closed).ToListAsync();
-//        }
+        public static IQueryable<UserTab> AllActiveTabsForUser(this IQueryable<UserTab> query, Guid userId)
+        {
+            return query
+                .Include(ut => ut.ContextPart)
+                .Where(ut => ut.UserId == userId && ut.Status == TabStatus.Open);
+        }
 
-//        public Task<IEnumerable<UserTab>> GetOpenTabsByContextIdAsync(int contextId)
-//        {
-//            throw new NotImplementedException();
-//        }
-
-//        public Task<IEnumerable<UserTab>> GetStatus()
-//        {
-//            throw new NotImplementedException();
-//        }
-
-//        //USer
-//        public async Task<IEnumerable<UserTab>> GetTabsByUserIdAsync(int userId)
-//        {
-//            //var user = await _dbSet.Include(u => u.User)
-//            //    .Where(u => u.UserId == userId)
-//            //    .Include(u => u.ContextPart)
-//            //    .Select(u => u.ContextPartId)
-//            //    .ToListAsync();
-
-
-//            return await _dbSet.Where(ut => ut.UserId == userId).ToListAsync();
-//        }
-//        public async Task<User> GetParticipantCountAsync(int userId)
-//        {
-//            throw new NotImplementedException();
-//        }
-
-//        public async Task<decimal> GetTotalAsync(int tabId)
-//        {
-//            return await _dbSet.Where(ut => ut.Id == tabId)
-//                .SelectMany(ut => ut.Transactions)
-//                .SumAsync(t => t.Product.Price);
-//        }
-
-//        //details for a tab thorugh transactions and payments from the id that is from the user tab
-//        public async Task<UserTab?> GetTabWithDetailsAsync(int id)
-//        {
-//            var tab = await _dbSet.Select(ut => ut)
-//                .Include(ut => ut.Transactions)
-//                .ThenInclude(t => t.Select(t => t.User.FirstName))
-//                .ToListAsync();
-//            //.Include(ut => ut.User)
-
-//            return tab.FirstOrDefault(ut => ut.Id == id);
-//        }
-
-//        public async Task<bool> IsTabClosedAsync(int tabId)
-//        {
-//            return await _dbSet.AnyAsync(ut => ut.Id == tabId && ut.Status == TabStatus.Closed);
-//        }
-
-//        public async Task<bool> IsTabOpenAsync(int tabId)
-//        {
-//            return await _dbSet.AnyAsync(ut => ut.Id == tabId && ut.Status == TabStatus.Open);
-//        }
-
-//        public async Task<bool> IsTabPaidAsync(int tabId)
-//        {
-//            return await _dbSet.AnyAsync(ut => ut.Id == tabId && ut.Status == TabStatus.Paid);
-//        }
-//    }
-//}
+        public static IQueryable<UserTab> FullDetail(this IQueryable<UserTab> query)
+        {
+            return query
+                .Include(ut => ut.Transactions)
+                .Include(ut => ut.Payments)
+                .Include(ut => ut.ContextPart)
+                .Include(ut => ut.User);
+        }
+    }
+}
