579041f5b4c8880fb1f6ec34666f3c1d5b3ac1b7
theMargarita <margotvoistinova@hotmail.se>
2026-02-16 15:53:53 +0100

    Refactor UserTab namespace and remove default timestamps

--- Domain/Models/UserTab.cs
+++ Domain/Models/UserTab.cs
@@ -1,14 +1,12 @@
-﻿using Domain.Models;
-
-namespace Domain.Models
+﻿namespace Domain.Models
 {
     public class UserTab //användar nota
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public Guid UserId { get; set; }
         public Guid ContextId { get; set; }
-        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
-        public DateTimeOffset ClosedAt { get; set; } = DateTimeOffset.UtcNow;
+        public DateTimeOffset CreatedAt { get; set; } //= DateTimeOffset.UtcNow;
+        public DateTimeOffset ClosedAt { get; set; } //= DateTimeOffset.UtcNow;
         public TabStatus Status { get; set; }
 
         public Guid? OwnerId { get; set; }
