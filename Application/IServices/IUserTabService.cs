4aca34d42abf1a0806aab72271350e9cb1979943
theMargarita <margotvoistinova@hotmail.se>
2026-02-16 15:54:39 +0100

    Update IUserTabService: use GUIDs, add ToOrder method

--- Application/IServices/IUserTabService.cs
+++ Application/IServices/IUserTabService.cs
@@ -1,24 +1,28 @@
-﻿//using Application.DTOs.UserTabFolder.AdminTabFolder;
-//using Application.DTOs.UserTabFolder.Resonse;
-//using Domain.Models;
+﻿using Application.DTOs.UserTabFolder.AdminTabFolder;
+using Application.DTOs.UserTabFolder.Request;
+using Application.DTOs.UserTabFolder.Resonse;
+using Application.DTOs.UserTabFolder.Response;
+using Domain.Models;
 
-//namespace Application.IServices
-//{
-//    public interface IUserTabService
-//    {
-//        Task<IEnumerable<ActiveTabSummary>> GetActiveTabAsync();
-//        Task<UserTabResponse?> GetByIdAsync(int id);
-//        Task<IEnumerable<UserTabResponse>> GetAllAsync();
-//        Task<UserTabResponse> CreateAsync(UserTab tab);
-//        Task<UserTabResponse> UpdateAsync(UserTab tab);
-//        Task<bool> DeleteAsync(int id);
-//        Task<UserDetailTabResponses?> GetTabDetailsAsync(int tabId);
 
-//        //to controll if user has opened a tab
-//        Task<bool> HasOpenTabAsync(int userId);
-//        //admin close the tab
-//        Task<bool> CloseTabAsync(int tabId);
-//        //check if the tab is paid
-//        Task<bool> IsTabPaidAsync(int tabId);
-//    }
-//}
+namespace Application.IServices
+{
+    public interface IUserTabService
+    {
+        Task<IEnumerable<ActiveTabSummary>> GetActiveTabAsync();
+        Task<UserTabResponse?> GetTabByIdAsync(Guid id);
+        Task<IEnumerable<UserTabResponse>> GetAllAsync();
+        Task<UserTabResponse> CreateAsync(Guid id);
+        Task<UserTabResponse> UpdateAsync(Guid id);
+        Task<bool> DeleteAsync(Guid id);
+        Task<UserDetailTabResponses?> GetTabDetailsAsync(Guid tabId);
+        Task<ScanRequest> ToOrder(string qrToken);
+
+        //to controll if user has opened a tab
+        Task<bool> HasOpenTabAsync(Guid userId);
+        //admin close the tab
+        Task<bool> CloseTabAsync(Guid tabId);
+        //check if the tab is paid
+        Task<bool> IsTabPaidAsync(Guid tabId);
+    }
+}
