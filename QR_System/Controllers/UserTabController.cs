464fcbe664fa3e20b46c0fc82fec88656ab9e39d
theMargarita <margotvoistinova@hotmail.se>
2026-02-16 14:27:57 +0100

    added  controller

---
+++ QR_System/Controllers/UserTabController.cs
@@ -0,0 +1,14 @@
+﻿using Application.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QR_System.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserTabController : ControllerBase
+    {
+        private readonly IUserTabService _tabService;
+        //private readonly 
+
+    }
+}
