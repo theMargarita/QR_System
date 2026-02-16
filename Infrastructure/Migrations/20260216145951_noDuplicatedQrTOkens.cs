591eddff2ca5a9f64330e50c16296996564cb868
theMargarita <margotvoistinova@hotmail.se>
2026-02-16 16:00:17 +0100

    new migration

---
+++ Infrastructure/Migrations/20260216145951_noDuplicatedQrTOkens.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class noDuplicatedQrTOkens : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_contextparts_QrToken",
+                table: "contextparts",
+                column: "QrToken",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_contextparts_QrToken",
+                table: "contextparts");
+        }
+    }
+}
